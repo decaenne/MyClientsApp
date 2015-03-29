857d7b5b1f8f1df1de23d96679cae4d6b3f5cdc4
USUARIO <alanbarreraf@hotmail.com>
2015-03-29 00:18:54 -0600

    Initial commit

---
+++ app/src/main/java/com/example/usuario/myclientsapp/menu2_Fragment.java
@@ -0,0 +1,20 @@
+package com.example.usuario.myclientsapp;
+import android.app.Fragment;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by USUARIO on 28/03/2015.
+ */
+public class menu2_Fragment extends Fragment {
+    View rootview;
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        rootview = inflater.inflate(R.layout.menu2_layout, container, false);
+        return rootview;
+    }
+}
